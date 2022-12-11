c422a2afcfb0942e32ce68541589c4a0388a635b
Jalil <rjalilbek578@gmail.com>
2022-12-11 20:59:42 +0500

    fix

--- pages/card.js
+++ pages/card.js
@@ -1,9 +1,11 @@
 import Card from "../components/Card/card"
 import styles from '../styles/Home.module.css'
 
+
 const CardPage = () =>{
     return(
         <div className={styles.wrapper}>
+             <h1>BOT</h1>
               <Card/>
         </div>
     )
